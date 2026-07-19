fc16fe7c9d87cd16ec4acf4ecddab2b7d991f95b
lavishtembhare <lavishtembhare93@gmail.com>
2026-07-20 00:15:19 +0530

    Update

--- src/main/java/com/main/Smart_Email_Assistant/app/SmartEmailAssistantController.java
+++ src/main/java/com/main/Smart_Email_Assistant/app/SmartEmailAssistantController.java
@@ -1,14 +1,15 @@
-package com.main.Smart_Email_Assistant;
+package com.main.Smart_Email_Assistant.app;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/email")
 public class SmartEmailAssistantController {
-    public ResponseEntity<String> generateEmail(){
-        
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        return ResponseEntity.ok("");
     }
 }
